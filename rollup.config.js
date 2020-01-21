c92df9afab1b47de5d28986682f8119ff0f44d5f
bultas <danielbultas@gmail.com>
2020-01-21 23:15:33 +0100

    rollup - resolve node_modules, install deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+import resolve from "@rollup/plugin-node-resolve";
+
 export default {
   input: "./src/index.js",
   output: {
     file: "./bundle.js",
     format: "iife"
-  }
+  },
+  plugins: [resolve()]
 };
